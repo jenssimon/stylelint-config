7a37ef91da107db4bb4adcff3c968dba605f5b50
Jens Simon <jens.simon80@googlemail.com>
2025-09-09 13:17:30 +0200

    fix: migrate to new version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,33 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-import { FlatCompat } from '@eslint/eslintrc'
-
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url) // eslint-disable-line no-underscore-dangle
-const __dirname = path.dirname(__filename) // eslint-disable-line no-underscore-dangle
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+import { configs } from '@jenssimon/eslint-config-base'
+import globals from 'globals'
 
 
 export default [
-  ...compat.config({
-    extends: [
-      '@jenssimon/eslint-config-base',
-    ],
-    rules: {
-      'unicorn/no-null': 'off',
-    },
-    overrides: [
-      {
-        files: ['*.js'],
-        rules: {
-          'unicorn/prefer-module': 'off',
-        },
-      },
-    ],
-  }).map((rule) => ({
-    files: [
-      '**/*.js',
-      '**/*.mjs',
-    ],
-    ...rule,
-  })),
   {
     ignores: [
       '.yarn/',
     ],
+  },
+
+  ...configs.base,
+
+  {
+    rules: {
+      'unicorn/no-null': 'off',
+    },
+  },
+
+  {
+    files: [
+      '**/*.js',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.commonjs,
+      },
+    },
+    rules: {
+      'unicorn/prefer-module': 'off',
+    },
   },
 ]
